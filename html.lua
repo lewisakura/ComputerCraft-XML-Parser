0197a7e885cbef6fce7e80cbe53bfadc7e77f2a7
lewislovesgames <lewisminecraft@hotmail.co.uk>
2015-01-02 16:55:42 +0000

    Create html.lua

---
+++ html.lua
@@ -0,0 +1,8 @@
+--[[--
+HTML Parser built by lewislovesgames.
+To add to your code, add this: dofile( "html" )
+That makes it so it's like running functions that are written in your code
+--]]--
+function html:parse( file )
+ local html = {}
+end
