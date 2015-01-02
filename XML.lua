c17de001d432ea5e9d6d2476aa55f8f6457ad1bc
lewislovesgames <lewisminecraft@hotmail.co.uk>
2015-01-02 16:58:21 +0000

    Update and rename html.lua to XML.lua

---
+++ XML.lua
@@ -0,0 +1,8 @@
+--[[--
+XML Parser built by lewislovesgames.
+To add to your code, add this: dofile( "XML" )
+That makes it so it's like running functions that are written in your code
+--]]--
+function xml:parse( file )
+ 
+end
